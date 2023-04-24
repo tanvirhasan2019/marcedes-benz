f1544cd1e04766faccdc4b161a005fd967f031db
tanvirhasan2019 <tanvirhasan.tanshen2019@gmail.com>
2023-04-24 14:41:38 +0600

    life style section redesign

--- src/components/gallery/MasonryImageList.js
+++ src/components/gallery/MasonryImageList.js
@@ -10,8 +10,8 @@ export default function MasonryImageList({itemData}) {
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
             <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}&w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}&w=248&fit=crop&auto=format&dpr=2 2x`}
               alt={item.title}
               loading="lazy"
             />
